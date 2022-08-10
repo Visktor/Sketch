386a48fd1cc9c88a19f94ccfb01d29cada3f14a8
Vitor <webdev.vitor@gmail.com>
2022-08-10 18:08:35 -0300

    Add shade function.
    I found a way to create a shade like effect working with rgb values.
    by taking these 3 values and subtracting 10% of the max rgb value (25),
    the color will get increasingly dark, but it's still not as accurate as
    it would be by running a conversion to HSL, which is a color value
    that takes lighting as it's own value. That would allow us to simply
    subtract 10 from the current light value and get a perfect shading
    effect. Right now, tho, that requires knowledge I believe I'm far
    from accquiring (I might be wrong) so I'll leave it as it is for now.
    
    This is a project I'd definitely like to revisit in the future, but for
    now I'm incredibly happy with the progress I've made knowledgewise
    while working the many problems that emerged.

--- index2.js
+++ index2.js
@@ -5,6 +5,8 @@ const pencil = document.querySelector(".cancel");
 const container = document.querySelector(".outer-container");
 const eraser = document.querySelector(".eraser");
 const inner = document.createElement("div");
+const rainbowRGB = document.querySelector(".rainbowrgb");
+const rainbowHEX = document.querySelector(".rainbowhex");
 inner.classList.add("inner-div");
 
 let defaultSize = 4;
@@ -13,6 +15,15 @@ let defaultColor = "black";
 let mode = defaultColor;
 let canvasSize = defaultSize;
 let elementQuantity = canvasSize ** 2;
+let mouseDown = false;
+
+//This keeps track of clicks on the entire page.
+document.body.onmousedown = () => {
+  mouseDown = true;
+};
+document.body.onmouseup = () => {
+  mouseDown = false;
+};
 
 popup.addEventListener("click", () => {
   let userSizeChoice = prompt("Choose Canvas Size - ");
@@ -26,6 +37,7 @@ popup.addEventListener("click", () => {
 
   if (Number(userSizeChoice)) {
     canvasSize = userSizeChoice;
+    elementQuantity = canvasSize ** 2;
     removeChildren();
     createNewElements();
   } else {
@@ -48,14 +60,26 @@ rainbowHSL.addEventListener("click", () => {
   changeMode();
 });
 
+rainbowRGB.addEventListener("click", () => {
+  mode = "rainbowRGB";
+});
+
+rainbowHEX.addEventListener("click", () => {
+  mode = "rainbowHEX";
+});
+
+shadow.addEventListener("click", () => {
+  mode = "shadeMode";
+});
+
 function removeChildren() {
-  let children = container.childNodes;
-  children.forEach((child) => {
-    container.removeChild(child); //removing previous children.
-  });
-}
+  while (container.childElementCount > 0) {
+    container.removeChild(container.childNodes[0]);
+  }
+} //removing previous children.
 
 function createNewElements() {
+  console.log(elementQuantity);
   for (x = 0; x < elementQuantity; x++) {
     container.appendChild(inner.cloneNode(true));
   }
@@ -67,12 +91,20 @@ function changeMode() {
   let children = container.childNodes;
   children.forEach((child) => {
     child.addEventListener("mouseover", () => {
-      if (mode === "black") {
-        child.style.backgroundColor = "black";
-      } else if (mode === "white") {
-        child.style.backgroundColor = "white";
-      } else if (mode === "rainbowHSL") {
-        child.style.backgroundColor = makeRainbowHSL();
+      if (mouseDown) {
+        if (mode === "black") {
+          child.style.backgroundColor = "black";
+        } else if (mode === "white") {
+          child.style.backgroundColor = "white";
+        } else if (mode === "rainbowHSL") {
+          child.style.backgroundColor = makeRainbowHSL();
+        } else if (mode === "rainbowRGB") {
+          child.style.backgroundColor = makeRainbowRGB();
+        } else if (mode === "rainbowHEX") {
+          child.style.backgroundColor = makerainbowHEX();
+        } else if (mode === "shadeMode") {
+          child.style.backgroundColor = shade(child);
+        }
       }
     });
   });
@@ -81,11 +113,77 @@ function changeMode() {
 function makeRainbowHSL() {
   let H = Math.floor(Math.random() * 360) + 1;
   let S = Math.floor(Math.random() * 100) + 1;
-  let L =
-   Math.floor(Math.random() * 100) + 1;
+  let L = Math.floor(Math.random() * 100) + 1;
   let A = 1;
   hslValue = `hsla(${H}, ${S}%, ${L}%, ${A})`;
   return hslValue;
 }
 
+function makeRainbowRGB() {
+  let R = Math.floor(Math.random() * 255) + 1;
+  let G = Math.floor(Math.random() * 255) + 1;
+  let B = Math.floor(Math.random() * 100) + 1;
+  let A = 1;
+  rgbValue = `rgba(${R}, ${G}, ${B}, ${A})`;
+  return rgbValue;
+}
+
+function makerainbowHEX() {
+  let randomColorString = "#";
+  const arrayOfColorFunctions = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+
+  for (let x = 0; x < 6; x++) {
+    let index = Math.floor(Math.random() * 16);
+    let value = arrayOfColorFunctions[index];
+
+    randomColorString += value;
+  }
+  return randomColorString;
+}
+
 (window.onload = createNewElements()), changeMode();
+
+// "rbg(xxx, xxx, xxx);"
+function shade(element) {
+  let oldbgColor = element.style.backgroundColor;
+  let rgbString = oldbgColor.slice(4, -1);
+  let rgbArray = rgbString.split(",");
+  let R = Number(rgbArray[0]);
+  if (R > 25) {
+    R -= 25;
+  } else {
+    R = 0;
+  }
+  let G = Number(rgbArray[1]);
+  if (G > 25) {
+    G -= 25;
+  } else {
+    G = 0;
+  }
+  let B = Number(rgbArray[2]);
+  if (B > 25) {
+    B -= 25;
+  } else {
+    B = 0;
+  }
+  let bgShade = `rgb(${R}, ${G}, ${B})`;
+  console.log(bgShade);
+  return bgShade;
+}
